ba09c87bdecd4ae1af1e51473e812149ada756d9
SamueleSamonini <samonini.samu@gmail.com>
2024-11-20 17:26:57 +0100

    comment the files 'trip' and 'app'
    comment the functions and the codes present in the files 'src/trip.py' and 'app.py'

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from src import csv_cleaner, graph, trip
 # Streamlit title and description
 st.title("European Temperature Analysis")
 
-# We create the sidebar menu using streamlit
+# create the sidebar menu using streamlit
 st.sidebar.title("Section")
 section = st.sidebar.radio("Choose a Section", ("Global Temperature Trends", "Europe Temperature Map", "Trip Calculator"))
 
-# we load the CSV file called GlobalTemperatures, and we clean it
+# load the CSV file called GlobalTemperatures, and we clean it
 csv_path = 'data/GlobalTemperatures.csv'
 data_cleaned = csv_cleaner.data_clean_global_temperatures(csv_path)
 
@@ -18,24 +18,28 @@ data_cleaned = csv_cleaner.data_clean_global_temperatures(csv_path)
 world = gpd.read_file("map/ne_10m_admin_0_countries.shp") 
 world = world.explode(index_parts=False)
 
-# We filter the file for print only the europe, and we delete russia and other far land for a better visualization of the map
+# filter the file for having only the europe, and we delete russia and other far land for a better visualization of the map
 europe = world[(world['CONTINENT'] == 'Europe') & (world['SOVEREIGNT'] != 'Russia')]
 europe = europe[(europe.geometry.centroid.y > 35) & (europe.geometry.centroid.y < 72) &
                 (europe.geometry.centroid.x > -30) & (europe.geometry.centroid.x < 50)]
 
+# change somo names of the countries in europe
 europe['SOVEREIGNT'] = europe['SOVEREIGNT'].replace({
     "Czechia": "Czech Republic",
     "Republic of Serbia": "Serbia"
 })
 
+# read the CSV
 europe_csv = pd.read_csv('data/europe_city.csv')
 
-# we must clean the cordinates, because it contains some char value like N for North
+# clean the cordinates, because it contains some char value like N for North
 europe_csv = csv_cleaner.clean_coordinates(europe_csv, lat_col = 'Latitude', lon_col = 'Longitude')
 
+# first section of the streamlit app
 if section == "Global Temperature Trends":
     st.header("Global Temperature Trends", divider = "gray")
 
+    # we use HTML and CSS to justify and display using bullet poin the data
     st.markdown("""
         <div style='text-align: justify;'>
             <b> In this section of the app, we use a dataframe containing global temperature data for each month from 1740 to 2015, which we visualize in an interactive graph. </b>
@@ -51,11 +55,10 @@ if section == "Global Temperature Trends":
     st.divider()
     st.write("We first clena the data, and after we call a function for create the plot: ")
 
+    # call the function and show the line chart
     complete_temperature_chart = graph.plot_line_trend(data_cleaned)
-
     st.altair_chart(complete_temperature_chart, use_container_width=True)
 
-    # we use HTML and CSS to justify and display using bullet poin the data
     st.markdown("""
         <div style='text-align: justify;'>
             <ul>
@@ -67,8 +70,10 @@ if section == "Global Temperature Trends":
         unsafe_allow_html = True
     )
 
+# second section of the streamlit app
 elif section == "Europe Temperature Map":
     st.header("Europe Temperature Map", divider = "gray")
+
     st.markdown("""
         <div style='text-align: justify;'>
             <b> In this section of the app, we use a GeoPandas world map and merge it with a dataframe containing extensive temperature data for cities and countries. We then display two interactive maps of Europe: the first shows the average temperature for each country, and the second highlights the 10 cities with the highest and lowest thermal excursions. </b>
@@ -77,28 +82,37 @@ elif section == "Europe Temperature Map":
         unsafe_allow_html = True
     )
 
-    # plot the europe, coloring the country accordingly to average temperature
+    # Group the data by unique countries in Europe, calculating the average of all historical temperature data
     state_branches = europe_csv.groupby('Country')['AverageTemperature'].mean().reset_index()
     countries_in_europe = europe.groupby('SOVEREIGNT')
 
     st.write("Using the data in our dataframe, we will analyze all temperature records and calculate an average for each country. These are the results:")
-    st.dataframe(state_branches, use_container_width=True)
+    st.dataframe(state_branches, use_container_width=True) # display the state_branches df
     st.write("Now, we display the data on an interactive map:")
 
+    # Merge the average temperature data (`state_branches`) into the Europe GeoDataFrame (`europe`) based on country names
+    # for this above we hange the names of some countries
     europe = europe.merge(state_branches, left_on="SOVEREIGNT", right_on="Country", how="left")
+    # Convert the coordinate reference system (CRS) of the GeoDataFrame to EPSG:4326, which is required for mapping
     europe = europe.to_crs(epsg=4326)
+    # Extract GeoJSON data from the GeoDataFrame for use in interactive map visualizations
     geojson_data = europe.__geo_interface__
 
+    # plot the europe, coloring the country accordingly to average temperature
     fig_interactive_map_temperature = graph.europe_temperature_map(europe, geojson_data)
-
     st.plotly_chart(fig_interactive_map_temperature)
 
     # Now we want to find the 10 cities with the highest/lower thermal excursion
+    # group all the unique cities, with other info, and we take the max and the min values of the AverageTemperature column
     city_te = europe_csv.groupby(['City', 'Country', 'Latitude', 'Longitude'])['AverageTemperature'].agg(['max', 'min']).reset_index()
+    # calculate the thermal excursion, subtract the min value of temperature from the max
     city_te['thermal_excursion'] = city_te['max'] - city_te['min']
+    # choose the columns
     city_te.columns = ['city', 'country', 'latitude', 'longitude', 'max_temp', 'min_temp', 'thermal_excursion']
 
+    # take the 10 cities with the largest thermal excursion
     top_10_highest_excursion = city_te.nlargest(10, 'thermal_excursion')
+    # take the 10 cities with the lowest thermal excursion
     top_10_lowest_excursion = city_te.nsmallest(10, 'thermal_excursion')
 
     st.markdown('''
@@ -107,15 +121,18 @@ elif section == "Europe Temperature Map":
         <div> <br>''',
         unsafe_allow_html = True
     )
-    st.dataframe(top_10_highest_excursion, use_container_width=True)
+    st.dataframe(top_10_highest_excursion, use_container_width=True) # display the 10 cities with the highest thermal excursion
     
     st.write("And the 10 cities with the lowest thermal excursion:")
-    st.dataframe(top_10_lowest_excursion, use_container_width=True)
+    st.dataframe(top_10_lowest_excursion, use_container_width=True) # display the 10 cities with the lowest thermal excursion
 
     st.write("Now, we display the data on an interactive map:")
+
+    # dispaly the interactive map of europe with the cities
     fig_interactive_map_thermal_excursion = graph.europe_thermal_excursion_map(europe, geojson_data, top_10_highest_excursion, top_10_lowest_excursion)
     st.plotly_chart(fig_interactive_map_thermal_excursion)
-    
+
+# third section of the streamlit app    
 elif section == "Trip Calculator":
     st.header("Trip Calculator Based on Temperature", divider = 'gray')
 
@@ -130,29 +147,33 @@ elif section == "Trip Calculator":
         unsafe_allow_html = True
     )
 
+    # take all the unique cities in europe_csv, and reverse it
     unique_cities_for_trip = europe_csv['City'].unique()
     reversed_cities_for_trip = unique_cities_for_trip[::-1]
 
+    # opens a selectbox in streamlit with all the cities present in the europe_csv df, for choosing the city where the trip start
     start_city = st.selectbox(
         "Select the start city: ",
-        unique_cities_for_trip,
+        unique_cities_for_trip, # display the unique cities in correct order
         key = "start_city_selectbox", # we need to put a key because streamlit give us an error if we don't put it
         placeholder = "No city selected"
     )
 
+    # opens a selectbox in streamlit with all the cities present in the europe_csv df, for choosing the city where the trip finish
     final_city = st.selectbox(
         "Select the start city: ",
-        reversed_cities_for_trip,
+        reversed_cities_for_trip, # display the unique cities in reversed order, only for graphical pourpose
         key = "final_city_selectbox",
         placeholder = "No city selected"
     )
 
+    # if also start_city and final_city are selected, calculate the trip
     if start_city and final_city:
         cities_trip = trip.trip_calculator(europe_csv, start_city, final_city)
-        st.write("Cities visited", cities_trip)
+        st.write("Cities visited", cities_trip) # print the trip
 
-        fig = graph.plot_trip_map(europe_csv, cities_trip)
         # Display the interactive map in Streamlit
+        fig = graph.plot_trip_map(europe_csv, cities_trip)
         st.plotly_chart(fig)
         
     else:
--- src/trip.py
+++ src/trip.py
@@ -1,17 +1,41 @@
 import numpy as np
 
 def trip_calculator(europe_csv, start_city, final_city):
-    recent_temperature_df = europe_csv[europe_csv['dt'] >= '2005-01-01']
-    europe_city = recent_temperature_df.groupby(['City', 'Latitude', 'Longitude'])['AverageTemperature'].mean().reset_index()
+    """
+    Create a fuction that, given in input a starting city, try the path for reaching a finla city, sercing for the three nearest cities,
+        and choose for next stage until the function reach the target city
 
-    print("Initial DataFrame:", europe_city.head())
+    Parameter: 
+        europe_csv -> the DataFrame containing city data including names, coordinates, and other attributes.
+        start_city -> the city where the trip start
+        final_city -> the city where the trip end
+
+    Return:
+        cities_already_visited -> return the ordered list of the trip, from the start city, all the stages, and ending with the final city
+    """
+    recent_temperature_df = europe_csv[europe_csv['dt'] >= '2005-01-01'] # filter only the recent data
+    
+    # create a df that contains unique cities with his coordinates adn average temeprature
+    europe_city = recent_temperature_df.groupby(['City', 'Latitude', 'Longitude'])['AverageTemperature'].mean().reset_index() 
 
     cities_already_visited = []
     city_deleted = set()
 
+    # a NumPy array that contain the coordinates of the cities
     city_coords = europe_city[['Latitude', 'Longitude']].values
 
     def distance_calculation(current_coordinates, cities_not_visited):
+        """
+        This function calculate the distance from the current cities to the other cities in cities_not_visited and return a list
+            containing the three nearest cities 
+
+        Parameter: 
+            current_coordinates -> the curdinates of the current city
+            cities_not_visited -> the cities not visited and not deleted
+
+        Return:
+            cities_not_visited.iloc[closest_indices]['City'].tolist() -> return a list with the three nearest cities from the current city
+        """
         # Calculate distances from the current city to all other cities in cities_not_visited
         distances = np.sqrt(
             (cities_not_visited['Latitude'].values - current_coordinates[0]) ** 2 +
@@ -24,15 +48,29 @@ def trip_calculator(europe_csv, start_city, final_city):
 
 
     def trip_3_cities(europe_city, cities_already_visited, final_city):
-        current_city = cities_already_visited[-1]
-        current_city_data = europe_city.query("City == @current_city")
-        current_coordinates = np.array([current_city_data['Latitude'].values[0], current_city_data['Longitude'].values[0]])
+        """
+        This function determines the next city to visit during a trip across Europe by considering the closest cities
+        and prioritizing the final destination if it's among them. If not, it selects the city with the highest average temperature.
+
+        Parameters:
+            europe_city -> DataFrame containing city data
+            cities_already_visited -> List of cities already visited in the trip
+            final_city -> The city that is the trip's final destination
+
+        Returns:
+            cities_already_visited -> Updates the list of visited cities by appending the next city to visit
+        """
+        current_city = cities_already_visited[-1] # the new current city is the last city in the list
+        current_city_data = europe_city.query("City == @current_city") # take the data of the current city
+        # take the coordinates of the city, latitude and longitude
+        current_coordinates = np.array([current_city_data['Latitude'].values[0], current_city_data['Longitude'].values[0]]) 
 
         # Filter out cities that are already visited or deleted
         cities_not_visited = europe_city[~europe_city['City'].isin(cities_already_visited + list(city_deleted))]
 
-        # Find the names of the 3 closest cities
+        # Find the names of the 3 closest cities using distance calculation
         closest_3_cities_names = distance_calculation(current_coordinates, cities_not_visited)
+        # Filter the DataFrame to include only the 3 closest cities
         closest_3_cities = cities_not_visited[cities_not_visited['City'].isin(closest_3_cities_names)]
 
         # Check if final_city is among the closest cities
@@ -43,22 +81,12 @@ def trip_calculator(europe_csv, start_city, final_city):
         else:
             # Otherwise, select the city with the highest temperature
             highest_temp_city = closest_3_cities.loc[closest_3_cities['AverageTemperature'].idxmax(), 'City']
-            cities_already_visited.append(highest_temp_city)
+            cities_already_visited.append(highest_temp_city) # and append the city in cities_already_visited
 
             # Mark the other two cities as deleted so they are not reconsidered
             for city in closest_3_cities['City']:
                 if city != highest_temp_city:
                     city_deleted.add(city)
-
-    # start_city = input('Insert start city: ')
-    # while start_city not in europe_city['City'].values:
-    #     print("Error, the city is not present in the df!!")
-    #     start_city = input('Insert start city: ')
-    
-    # final_city = input('Insert final city: ')
-    # while final_city not in europe_city['City'].values:
-    #     print("Error, the city is not present in the df!!")
-    #     final_city = input('Insert start city: ')
         
     cities_already_visited.append(start_city)
 
@@ -66,11 +94,13 @@ def trip_calculator(europe_csv, start_city, final_city):
     max_iterations = 500
     iteration = 0
 
+    # continue the trip until the final_city is not in cities_already_visited, in that case we'll conclude the trip
     while final_city not in cities_already_visited and iteration < max_iterations:
-        print(f"Iteration {iteration}: Currently at {cities_already_visited[-1]}")
-        trip_3_cities(europe_city, cities_already_visited, final_city)
+        print(f"Iteration {iteration}: Currently at {cities_already_visited[-1]}") # print out the current city
+        trip_3_cities(europe_city, cities_already_visited, final_city) # call the function
         iteration += 1
 
+    # check if final_city is present in cities_already_visited, in that case we have a trip
     if final_city in cities_already_visited:
         print(f"Reached {final_city}!")
     else:
